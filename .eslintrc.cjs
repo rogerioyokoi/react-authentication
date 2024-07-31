3ad2bb41538dd0c7967d3ae2900384bb937281e1
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-07-31 06:21:55 -0300

    Chore: config eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'lefthook.yml', 'package*.json', 'README.md', 'index.html'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'react'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'prettier/prettier': 'warn',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
